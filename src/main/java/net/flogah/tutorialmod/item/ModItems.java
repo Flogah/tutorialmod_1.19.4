5c2c0c387da392faae0f927c9986d1a54024c20d
Flogah <89162350+Flogah@users.noreply.github.com>
2023-06-16 15:56:23 +0200

    added citrine

--- src/main/java/net/flogah/tutorialmod/item/ModItems.java
+++ src/main/java/net/flogah/tutorialmod/item/ModItems.java
@@ -3,6 +3,7 @@ package net.flogah.tutorialmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.flogah.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -11,6 +12,32 @@ import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
 public class ModItems {
 
+	public static final Item CITRINE = registerItem("citrine",
+		new Item(new QuiltItemSettings()));
+
+	public static final Item RAW_CITRINE = registerItem("raw_citrine",
+		new Item(new QuiltItemSettings()));
+
+	private static Item registerItem(String name, Item item){
+		return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+	}
+
+	public static void registerModItems(){
+		TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
+	}
+
+	public static void addItemsToItemGroup(){
+		addToItemGroup(ItemGroups.INGREDIENTS, CITRINE);
+		addToItemGroup(ItemGroups.INGREDIENTS, RAW_CITRINE);
+	}
+
+	private static void addToItemGroup(ItemGroup group, Item item){
+		ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.addItem(item));
+
+		addItemsToItemGroup();
+	}
+
+	/*
 	public static final Item RAW_TANZANITE = registerItem("raw_tanzanite",
 		new Item(new QuiltItemSettings()));
 	public static final Item TANZANITE = registerItem("tanzanite",
@@ -25,4 +52,6 @@ public class ModItems {
 			content.addItem(TANZANITE);
 		});
 	}
+
+	 */
 }
