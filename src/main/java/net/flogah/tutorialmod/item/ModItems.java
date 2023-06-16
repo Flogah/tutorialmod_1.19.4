394e6157c862c08f0cb3579efaad4c96d1505af0
Flogah <89162350+Flogah@users.noreply.github.com>
2023-06-16 14:42:02 +0200

    initial commit again i guess

--- src/main/java/net/flogah/tutorialmod/item/ModItems.java
+++ src/main/java/net/flogah/tutorialmod/item/ModItems.java
@@ -1,22 +1,28 @@
 package net.flogah.tutorialmod.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.flogah.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
 public class ModItems {
 
-    public static final Item RAW_TANZANITE = registerItem("raw_tanzanite",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
-    public static final Item TANZANITE = registerItem("tanzanite",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
-    private static Item registerItem(String name, Item item){
-        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
-    }
-    public static void registerModItems(){
-        TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
-    }
+	public static final Item RAW_TANZANITE = registerItem("raw_tanzanite",
+		new Item(new QuiltItemSettings()));
+	public static final Item TANZANITE = registerItem("tanzanite",
+		new Item(new QuiltItemSettings()));
+	private static Item registerItem(String name, Item item){
+		return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+	}
+	public static void registerModItems(){
+		TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL_BLOCKS).register(content -> {
+			content.addItem(RAW_TANZANITE);
+			content.addItem(TANZANITE);
+		});
+	}
 }
